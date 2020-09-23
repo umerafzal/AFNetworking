91f6d12bb90484ce4f45607303616775ccb1eb51
Umer Afzal <umer.afzal07@gmail.com>
2020-09-23 14:14:01 +0500

    UIWebView code removed from AFNetworking

---
+++ UIKit+AFNetworking/WKWebView+AFNetworking.h
@@ -0,0 +1,19 @@
+#import <Foundation/Foundation.h>
+
+#import <Availability.h>
+
+#if defined(__IPHONE_OS_VERSION_MIN_REQUIRED)
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+
+@interface WKWebView (AFNetworking)
+
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+#endif
